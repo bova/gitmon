4deca0dff0891358beaf328e3deb7d1229eb6616
Bova <vladimir@fido.uz>
2014-08-29 12:11:49 +0500

    initial commit

---
+++ docs/install.rst
@@ -0,0 +1,2 @@
+pip install gitpython
+pip install colorama
---
+++ gitmon/checkrepos.py
@@ -0,0 +1,34 @@
+from git import *
+import colorama
+
+REPOS_LIST_FILE = 'repo.list'
+REPOS = []
+
+
+def is_repo_dirty(path):
+	repo = Repo(path)
+	return repo.is_dirty()
+
+def load_repo_path():
+	for path in open(REPOS_LIST_FILE).readlines():
+		path = path.rstrip('\n')
+		REPOS.append(path)
+
+
+def show_repo_status(path, is_dirty):
+	if is_dirty:
+		color = colorama.Fore.RED
+		status = 'DIRTY'
+	else:
+		color = colorama.Fore.GREEN
+		status = 'CLEAR'
+	print(colorama.Fore.YELLOW + path + ' ' + color + status)
+
+if __name__ == '__main__':
+	colorama.init()
+	load_repo_path()	
+	for path in REPOS:
+		is_dirty = is_repo_dirty(path)
+		show_repo_status(path, is_dirty)
+
+
